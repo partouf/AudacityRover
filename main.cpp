73c9d5cad65edb095a5591816ba59aaefdc69090
partouf <partouf@gmail.com>
2016-03-12 04:07:44 +0100

    setup main loop with comm, watchdog, cmd processing

--- main.cpp
+++ main.cpp
@@ -1,25 +1,72 @@
 
 #include <iostream>
 
+#include <Groundfloor/Atoms/Defines.h>
+#include <Groundfloor/Atoms/Initialize.h>
+
 #include "Command/CommandQueueDefault.h"
 #include "Communication/CommunicationJumpropes.h"
 
+#include <Jumpropes/Initialize.h>
+#include <Jumpropes/Functions.h>
+
+#include "System/WatchCat.h"
+
 OpenALRF::CommandQueue Queue;
-OpenALRF::CommunicationJumpropes Comm(&Queue);
+OpenALRF::ISystem *System = Queue.GetSystem();
+
+void RebootSystem()
+{
+   Queue.Add({ OpenALRF::modSystem, OpenALRF::actSystemReboot });
+   Queue.Process();
+}
 
 int main()
 {
-   std::cout << "Hello, World!" << std::endl;
+   std::cout << "Hello" << std::endl;
 
-   OpenALRF::Command Hello{ OpenALRF::modRemotePilot, OpenALRF::actRemotePilotForward, 2, 0 };
-   Queue.Add(Hello);
+   if (Groundfloor::initGroundfloor())
+   {
+      std::cout << "World!" << std::endl;
 
-   OpenALRF::Command World{ OpenALRF::modCamera, OpenALRF::actCameraCapture, 0, 0 };
-   Queue.Add(World);
+      if (Jumpropes::initJumpropes())
+      {
+         std::cout << "And Universe!" << std::endl;
 
+         AudacityRover::WatchCat Cat(System, &Queue);
+         AudacityRover::CommunicationJumpropes Comm(&Queue);
+         try
+         {
+            while (!System->ShouldQuit())
+            {
+               Comm.Process();
+               Queue.Process();
+               Cat.Process();
 
-   Queue.Process();
-   Queue.Process();
+               GFMillisleep(10);
+            }
+
+         }
+         catch (std::exception &e)
+         {
+            std::cout << e.what() << std::endl;
+         }
+
+         std::cout << "Goodbye!" << std::endl;
+
+         /*
+         OpenALRF::Command Hello{ OpenALRF::modRemotePilot, OpenALRF::actRemotePilotForward, 2, 0 };
+         Queue.Add(Hello);
+
+         OpenALRF::Command World{ OpenALRF::modCamera, OpenALRF::actCameraCapture, 0, 0 };
+         Queue.Add(World);
+         */
+
+         Jumpropes::finiJumpropes();
+      }
+
+      Groundfloor::initGroundfloor();
+   }
 
    return 1;
 }
