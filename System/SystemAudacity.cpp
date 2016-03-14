de448b03ce30b3ae4e07686e6a048775fd77ca2a
partouf <partouf@gmail.com>
2016-03-14 22:40:48 +0100

    debugging, because.

--- System/SystemAudacity.cpp
+++ System/SystemAudacity.cpp
@@ -13,12 +13,15 @@ bool AudacityRover::SystemAudacity::HasValidActiveNetwork()
    auto list = Jumpropes::ListNetworkInterfaces();
    for (auto netif : list)
    {
+      std::cout << netif.IP.ip.getValue() << std::endl;
+
       if (!netif.IP.ip.startsWith_ansi("127.") && !netif.IP.ip.startsWith_ansi("0.") && !netif.IP.ip.startsWith_ansi("169.254."))
       {
          HasNetwork = true;
          break;
       }
    }
+   std::cout << "-----" << std::endl;
 
    return HasNetwork;
 }
