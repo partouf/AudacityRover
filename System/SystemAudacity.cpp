0bb94f14c5ee3942121acec55d0dc00dafa6620c
partouf <partouf@gmail.com>
2016-03-14 23:19:36 +0100

    so that's why

--- System/SystemAudacity.cpp
+++ System/SystemAudacity.cpp
@@ -10,18 +10,16 @@ bool AudacityRover::SystemAudacity::HasValidActiveNetwork()
 {
    bool HasNetwork = false;
 
+   // note for RaspberryPi; you need to remove the 127.0.1.1 line in the /etc/hosts file as it will confuse this into thinking that's the only IP address we have
    auto list = Jumpropes::ListNetworkInterfaces();
    for (auto netif : list)
    {
-      std::cout << netif.IP.ip.getValue() << std::endl;
-
       if (!netif.IP.ip.startsWith_ansi("127.") && !netif.IP.ip.startsWith_ansi("0.") && !netif.IP.ip.startsWith_ansi("169.254."))
       {
          HasNetwork = true;
          break;
       }
    }
-   std::cout << "-----" << std::endl;
 
    return HasNetwork;
 }
