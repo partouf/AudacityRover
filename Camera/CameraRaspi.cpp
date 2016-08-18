f084d3e81d8e0d1591f997a87ca927af79cb61f2
partouf <partouf@gmail.com>
2016-08-18 16:56:36 +0200

    use raspicam library to take pictures

--- Camera/CameraRaspi.cpp
+++ Camera/CameraRaspi.cpp
@@ -4,18 +4,66 @@
 #include <string>
 #include <OpenALRF/Common/Timing.h>
 
+#include <Groundfloor/Atoms/Defines.h>
+
+#ifdef GF_OS_WIN32
+#include "raspicam_still_cvcam.h"
+#else
+#include "raspicam_still.h"
+#endif
+
+#include <OpenALRF/ImageProcessing/Software.h>
+
 AudacityRover::CameraRaspi::CameraRaspi() : OpenALRF::IMainCamera()
 {
+#ifdef GF_OS_WIN32
+   Cam = new raspicam::RaspiCam_Still_CVCam();
+#else
+   Cam = new raspicam::RaspiCam_Still();
+#endif
 
+   Cam->open();
+
+   InitializeImage();
+}
+
+void AudacityRover::CameraRaspi::InitializeImage()
+{
+   // todo: how to find out camera/output resolution
+   LatestImage.Width = 640;
+   LatestImage.Height = 480;
+   LatestImage.Channels = 3;
+
+   long BufferLen = LatestImage.Width * LatestImage.Height * LatestImage.Channels;
+
+   LatestImage.Buffer = new unsigned char[BufferLen];
+   LatestImage.BufferLength = BufferLen;
+}
+
+AudacityRover::CameraRaspi::~CameraRaspi()
+{
+   delete Cam;
+   delete LatestImage.Buffer;
 }
 
 void AudacityRover::CameraRaspi::TakePicture()
 {
-   // todo: use library
+   Cam->grab_retrieve(LatestImage.Buffer, LatestImage.BufferLength);
 
+   OpenALRF::ImageProcessingSoftware Stuff;
+   Stuff.Initialize(LatestImage);
+   Stuff.MakeObjectMask();
+
+/*
    auto ts = OpenALRF::GetCurrentTimestamp();
    std::string cmd = "raspistill -vf -o " + std::to_string(ts) + ".png";
    system(cmd.c_str());
+*/
+}
+
+OpenALRF::CapturedImage AudacityRover::CameraRaspi::GetLatestImage()
+{
+   return LatestImage;
 }
 
 std::string AudacityRover::CameraRaspi::GetStatusInfo()
--- Camera/CameraRaspi.h
+++ Camera/CameraRaspi.h
@@ -1,15 +1,25 @@
 #pragma once
 
 #include <OpenALRF/Camera/MainCamera.h>
+#include <OpenALRF/Common/CapturedImage.h>
+#include "raspicam_still_interface.h"
 
 namespace AudacityRover
 {
    class CameraRaspi : public OpenALRF::IMainCamera
    {
+   protected:
+      raspicam::RaspiCam_Still_Interface *Cam;
+      OpenALRF::CapturedImage LatestImage;
+
+      void InitializeImage();
    public:
       CameraRaspi();
+      ~CameraRaspi();
 
-      void TakePicture();
+      virtual void TakePicture() override;
+
+      virtual OpenALRF::CapturedImage GetLatestImage() override;
 
       virtual std::string GetStatusInfo() override;
    };
